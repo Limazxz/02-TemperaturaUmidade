f25836308f9bf78efb920a6b206449733bae4975
limazxz <goubulsegue@gmail.com>
2025-08-26 10:36:08 -0300

    Add initial files and configurations for various modules

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from "./contexts/ThemeContext.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
